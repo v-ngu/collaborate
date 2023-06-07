d75180801276f161f4dbea22b912336464cf809c
Viet Nguyen <19592926+v-ngu@users.noreply.github.com>
2023-06-07 14:15:26 -0400

    Create custom hook useHeader

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,37 +1,23 @@
 import { useState, useEffect } from "react";
-import { useAuth0 } from '@auth0/auth0-react';
 import makeFetchRequest from "../utils/make-fetch-request";
-
-const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+import useHeaders from "./useHeaders";
 
 const useFetch = (api, extraData) => {
   // states
   const [state, setState] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [headers, isLoadingHeaders] = useHeaders();
 
-  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
-  
-  // effect: the access token is retreived from Auth0 for 
-  // authentification before making a fetch request to the server
   useEffect(() => {
     (async () => {
-      if (isAuthenticated) {
-        const accessToken = await getAccessTokenSilently({
-          authorizationParams: { audience: audience, }
-        });
-        const headers = {
-          "authorization": `Bearer ${accessToken}`,
-          "content-type": "application/json",
-        };
-
+      if (!isLoadingHeaders) {
         const data = await makeFetchRequest(() => api(headers, extraData));
-
         setState(data);
         setIsLoading(false);
       }
     })();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isAuthenticated]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoadingHeaders]);
 
   return ([state, isLoading, setState]);
 };
---
+++ src/hooks/useHeaders.js
@@ -0,0 +1,35 @@
+// create a request headers with appropriate
+// access token for authentification
+
+import { useState, useEffect } from "react";
+import { useAuth0 } from '@auth0/auth0-react';
+
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+
+const useHeaders = () => {
+  const [headers, setHeaders] = useState({});
+  const [isLoadingHeaders, setIsLoadingHeaders] = useState(true);
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    (async () => {
+      if (isAuthenticated) {
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: { audience: audience, }
+        });
+
+        setHeaders({
+          "authorization": `Bearer ${accessToken}`,
+          "content-type": "application/json",
+        })
+
+        setIsLoadingHeaders(false);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated])
+
+  return [headers, isLoadingHeaders];
+};
+
+export default useHeaders;
