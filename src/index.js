d2df7df6cbbdd07e1b7a82b973b7b98cba516949
Viet Nguyen <19592926+v-ngu@users.noreply.github.com>
2023-06-18 01:51:24 -0400

    Refactor index

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { ProfileProvider } from './contexts/ProfileContext';
 
-// constants
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
-const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+// environment constants
+const {
+  REACT_APP_AUTH0_DOMAIN: domain,
+  REACT_APP_AUTH0_CLIENT_ID: clientId,
+  REACT_APP_AUTH0_CALLBACK_URL: redirectUri,
+  REACT_APP_AUTH0_AUDIENCE: audience
+} = process.env;
 
 // root rendering
 const root = ReactDOM.createRoot(document.getElementById('root'));
