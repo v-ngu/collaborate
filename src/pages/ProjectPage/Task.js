19953550de7377c20d73295fb7a0cc5f057cde85
Viet Nguyen <19592926+v-ngu@users.noreply.github.com>
2023-07-13 14:51:23 -0400

    Fix dragging bug

--- src/pages/ProjectPage/Task.js
+++ src/pages/ProjectPage/Task.js
@@ -1,15 +1,16 @@
 import { styled } from "styled-components";
+import { createPortal } from 'react-dom';
 import { Draggable } from "react-beautiful-dnd";
 import { useTaskDrawercontext } from "../../contexts/TaskDrawerContext";
 
 const Task = ({ taskObject, index }) => {
-  const { 
-    taskId, 
+  const {
+    taskId,
     task,
     assignee,
     dueDate
   } = taskObject;
-  
+
   const { setIsDrawerOpen, setDrawerContent } = useTaskDrawercontext();
 
   const handleClick = () => {
@@ -17,21 +18,35 @@ const Task = ({ taskObject, index }) => {
     setDrawerContent(taskObject);
   };
 
+  const renderComponent = (provided, snapshot) => {
+    const isDragging = snapshot.isDragging;
+
+    const child = (
+      <Wrapper
+        onClick={handleClick}
+        ref={provided.innerRef}
+        $isDragging={isDragging}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+      >
+        <p>{task}</p>
+        <p>{assignee}</p>
+        <p>{dueDate}</p>
+      </Wrapper>
+    );
+    
+    // a portal is needed to offset the transformation
+    // effect during the dragging stage
+    return (
+      isDragging
+        ? createPortal(child, document.body)
+        : child
+    );
+  };
+
   return (
     <Draggable draggableId={taskId} index={index}>
-      {(provided, snapshot) => (
-        <Wrapper
-          ref={provided.innerRef}
-          $isDragging={snapshot.isDragging}
-          onClick={handleClick}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          <p>{task}</p>
-          <p>{assignee}</p>
-          <p>{dueDate}</p>
-        </Wrapper>
-      )}
+      {(provided, snapshot) => renderComponent(provided, snapshot)}
     </Draggable>
   );
 };
