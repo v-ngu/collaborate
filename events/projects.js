d716b984d05f8fca346621bb4f3e40d016440134
Viet Nguyen <19592926+v-ngu@users.noreply.github.com>
2023-07-15 19:39:22 -0400

    Update project db on socket

--- events/projects.js
+++ events/projects.js
@@ -30,11 +30,17 @@ const registerProjectsHandlers = (io, socket) => {
     socket.to(socket.activeRoom).emit('projects:dnd-updated', { projectLists });
   };
 
+  const onUpdate = async ({ projectId, field, formData }) => {
+    await client.updateProject(projectId, field, formData);
+    io.to(socket.activeRoom).emit('projects:updated', { field, formData });
+  };
+
   // listeners and emitters
   socket.on('projects:add-task', onAddTask);
   socket.on('projects:add-user', onAddUser);
   socket.on('projects:remove-user', onRemoveUser);
   socket.on('projects:task-dragged', onTaskDragged);
+  socket.on('projects:update', onUpdate);
 };
 
 module.exports = registerProjectsHandlers;
