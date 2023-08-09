0007e940203fbe288927b66a346b3399725758da
Viet Nguyen <19592926+v-ngu@users.noreply.github.com>
2023-08-09 18:23:35 -0400

    Refactor project controller

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -1,67 +1,94 @@
-const Project = require('../models/ProjectSchema');
-const { StatusCodes } = require('http-status-codes');
+const Project = require("../models/ProjectSchema");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const client = require("../db/dbHandler");
 
-const client = require('../db/dbHandler');
+/**
+ * Controllers related to projects collection
+ *
+ */
 
-// controllers related to projects collection
+// Create new project
 const createProject = async (req, res) => {
   const { userId } = req.body;
 
-  const createdProject = await client.createProject(new Project(userId));
+  // Look if user exists
+  if (!(Boolean(await client.findUser(userId)))) {
+    throw new BadRequestError("Cannot create a project with non existing user");
+  }
 
-  const createdProjectId = createdProject.insertedId
+  // Create project
+  const createdProject = await client.createProject(new Project(userId));
+  const createdProjectId = createdProject.insertedId;
   console.log(`New project created with id: ${createdProjectId}`);
 
+  // Response with created project ID
   res.status(StatusCodes.CREATED).json({
-    status: StatusCodes.CREATED, data: createdProjectId
+    status: StatusCodes.CREATED,
+    data: { projectId: createdProjectId },
   });
 };
 
+// Get a specific project with ID
 const getProject = async (req, res) => {
   const { projectId } = req.params;
   const project = await client.findProject(projectId);
 
   res.status(StatusCodes.ACCEPTED).json({
-    status: StatusCodes.ACCEPTED, data: project
+    status: StatusCodes.ACCEPTED,
+    data: project,
   });
 };
 
+// Get all projects created by a user
 const getAllProjectsFromUser = async (req, res) => {
   const { userId } = req.params;
+
+   // Look if user exists
+   if (!(Boolean(await client.findUser(userId)))) {
+     throw new BadRequestError("User does not exists");
+   }
+
+  // Get all projects and return
   const allProjects = await client.findAllProjectsFromUser(userId);
 
   res.status(StatusCodes.ACCEPTED).json({
-    status: StatusCodes.ACCEPTED, data: allProjects
+    status: StatusCodes.ACCEPTED,
+    data: allProjects,
   });
 };
 
+// Get all projects that a user has access authorization
 const getAuthorizedProjectsForUser = async (req, res) => {
   const { userId } = req.params;
   const sharedProjects = await client.findAuthorizedProjectsForUser(userId);
 
   res.status(StatusCodes.ACCEPTED).json({
-    status: StatusCodes.ACCEPTED, data: sharedProjects
+    status: StatusCodes.ACCEPTED,
+    data: sharedProjects,
   });
 };
 
+// Update a project
 const updateProject = async (req, res) => {
   const { projectId } = req.params;
   const { field, data } = req.body;
   await client.updateProject(projectId, field, data);
-
+  console.log(response)
   res.status(StatusCodes.ACCEPTED).json({
-    status: StatusCodes.ACCEPTED, 
-    data: { msg: "Project updated" }
+    status: StatusCodes.ACCEPTED,
+    data: { msg: "Project updated" },
   });
 };
 
+// Delete a project
 const deleteProject = async (req, res) => {
   const { projectId } = req.params;
   await client.deleteProject(projectId);
 
   res.status(StatusCodes.ACCEPTED).json({
     status: StatusCodes.ACCEPTED,
-    data: { msg: "Project deleted" }
+    data: { msg: "Project deleted" },
   });
 };
 
@@ -71,5 +98,5 @@ module.exports = {
   getAllProjectsFromUser,
   getAuthorizedProjectsForUser,
   updateProject,
-  deleteProject
+  deleteProject,
 };
--- server/db/dbHandler.js
+++ server/db/dbHandler.js
@@ -1,7 +1,7 @@
-require('dotenv').config({ path: '../.env' });
-const { MongoClient, ObjectId } = require('mongodb');
-const { NotFoundError } = require('../errors');
-const { findTeamMembersPipeline } = require('./pipelines');
+require("dotenv").config({ path: "../.env" });
+const { MongoClient, ObjectId } = require("mongodb");
+const { NotFoundError } = require("../errors");
+const { findTeamMembersPipeline } = require("./pipelines");
 
 /**
  *  Database handler is used to handle MongoDB and its connection
@@ -10,17 +10,17 @@ const { findTeamMembersPipeline } = require('./pipelines');
 class DatabaseHandler {
   constructor() {
     this.client = new MongoClient(process.env.MONGO_URI);
-    this.db = this.client.db('collaborate');
+    this.db = this.client.db("collaborate");
 
     // databse collections
-    this.users = this.db.collection('users');
-    this.projects = this.db.collection('projects');
-  };
+    this.users = this.db.collection("users");
+    this.projects = this.db.collection("projects");
+  }
 
   // connection method
   connect() {
     this.client.connect();
-  };
+  }
 
   // users collections handlers
   async findTeamMembers(projectId) {
@@ -31,7 +31,7 @@ class DatabaseHandler {
 
   async findUser(id) {
     return await this.users.findOne({ _id: id });
-  };
+  }
 
   async createUser(newUser) {
     return await this.users.insertOne(newUser);
@@ -40,41 +40,41 @@ class DatabaseHandler {
   // projects collections handlers
   async createProject(newProject) {
     return await this.projects.insertOne(newProject);
-  };
+  }
 
   async findProject(projectId) {
     const result = await this.projects.findOne(new ObjectId(projectId));
-    if (!result) throw new NotFoundError(`No project with id ${projectId}`)
+    if (!result) throw new NotFoundError(`No project with id ${projectId}`);
     return result;
-  };
+  }
 
   async findAllProjectsFromUser(userID) {
     const cursor = await this.projects.find({ createdBy: userID });
     const results = await cursor.toArray();
     return results;
-  };
+  }
 
   async findAuthorizedProjectsForUser(userId) {
     const cursor = await this.projects.find({ authorizedUsers: userId });
     const results = await cursor.toArray();
     return results;
-  };
+  }
 
   async addTask(projectId, column, body) {
     const result = await this.projects.updateOne(
-      { _id: new ObjectId(projectId), 'projectLists.column': column },
+      { _id: new ObjectId(projectId), "projectLists.column": column },
       {
         $push: {
-          'projectLists.$.tasks': {
+          "projectLists.$.tasks": {
             $each: [body],
-            $position: 0
-          }
-        }
+            $position: 0,
+          },
+        },
       }
-    )
+    );
     if (!result) throw new NotFoundError(`No project with id ${projectId}`);
     return result;
-  };
+  }
 
   async updateProject(projectId, field, data) {
     const result = await this.projects.updateOne(
@@ -83,30 +83,32 @@ class DatabaseHandler {
     );
     if (!result) throw new NotFoundError(`No project with id ${projectId}`);
     return;
-  };
+  }
 
   async addAuthorizedUser(projectId, userId) {
     const result = await this.projects.updateOne(
       { _id: new ObjectId(projectId) },
-      { $push: { 'authorizedUsers': userId } }
+      { $push: { authorizedUsers: userId } }
     );
     if (!result) throw new NotFoundError(`No project with id ${projectId}`);
     return;
-  };
+  }
 
   async removeAuthorizedUser(projectId, userId) {
     const result = await this.projects.updateOne(
       { _id: new ObjectId(projectId) },
-      { $pull: { 'authorizedUsers': userId } }
-    )
-  };
+      { $pull: { authorizedUsers: userId } }
+    );
+  }
 
   async deleteProject(projectId) {
-    const result = await this.projects.deleteOne({ _id: new ObjectId(projectId) });
+    const result = await this.projects.deleteOne({
+      _id: new ObjectId(projectId),
+    });
     if (!result) throw new NotFoundError(`No project with id ${projectId}`);
     return result;
-  };
-};
+  }
+}
 
 const client = new DatabaseHandler();
 
