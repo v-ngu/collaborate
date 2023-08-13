1f965ef1535b562fdd78340b7597adbf4a55d884
Viet Nguyen <19592926+v-ngu@users.noreply.github.com>
2023-08-13 12:23:03 -0400

    Refactor TeamMember

--- client/src/pages/ProjectPage/TeamMembers.js
+++ client/src/pages/ProjectPage/TeamMembers.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useProjectContext } from "../../contexts/ProjectContext";
 import ProfileAvatar from "../../components/ProfileAvatar";
-import AvatarGroup from '@mui/material/AvatarGroup';
+import AvatarGroup from "@mui/material/AvatarGroup";
 import TeamMembersMenu from "./TeamMembersMenu";
 
 const TeamMembers = () => {
@@ -11,33 +11,29 @@ const TeamMembers = () => {
   // rendering
   return (
     <>
-      <AvatarGroup max={3} onClick={(event) => setAnchorEl(event.currentTarget)}>
-        {
-          teamMembers.map((teamMember, index) => {
-            const colors = ["#EB6868", "#526DC6", "#AD67CC", "#5A9B7E"];
-            const {
-              _id,
-              firstName,
-              lastName,
-              isOwner,
-              isAuthorized
-            } = teamMember;
+      <AvatarGroup
+        max={3}
+        onClick={(event) => setAnchorEl(event.currentTarget)}
+      >
+        {teamMembers.map((teamMember, index) => {
+          const colors = ["#EB6868", "#526DC6", "#AD67CC", "#5A9B7E"];
+          const { _id, firstName, lastName, isOwner, isAuthorized } =
+            teamMember;
 
-            if (!isAuthorized && !isOwner) return null;
+          if (!isAuthorized && !isOwner) return null;
 
-            return (
-              <ProfileAvatar
-                key={`PA${_id}`}
-                firstName={firstName}
-                lastName={lastName}
-                size="small"
-                bgColor={colors[index]}
-              />
-            );
-          })
-        }
+          return (
+            <ProfileAvatar
+              key={`PA${_id}`}
+              firstName={firstName}
+              lastName={lastName}
+              size="small"
+              bgColor={colors[index]}
+            />
+          );
+        })}
       </AvatarGroup>
-      <TeamMembersMenu anchorEl={anchorEl} setAnchorEl={setAnchorEl}/>
+      <TeamMembersMenu anchorEl={anchorEl} setAnchorEl={setAnchorEl} />
     </>
   );
 };
